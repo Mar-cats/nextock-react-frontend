e684e93deb705c5e6c26689132a8efb9df71721b
c9u11 <tjrdud6412@naver.com>
2025-02-02 17:41:29 +0900

    🧑‍💻 constants 및 기본 페이지 설정

---
+++ src/constants/routes.js
@@ -0,0 +1,5 @@
+
+export const ROUTES_BASENAME = import.meta.env.BASE_URL
+export const ROUTES_PATH_ROOT = "/"
+export const ROUTES_PATH_HOME = "/home"
+export const ROUTES_PATH_SECTOR = '/sector'
--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -1,11 +1,23 @@
-import { Routes, Route, BrowserRouter } from "react-router-dom";
+import { Routes, Route, BrowserRouter, Navigate } from "react-router-dom";
+import {
+  ROUTES_BASENAME,
+  ROUTES_PATH_ROOT,
+  ROUTES_PATH_HOME,
+  ROUTES_PATH_SECTOR
+} from "../constants/routes";
+
+const RootNavigator = () => {
+  return <Navigate to={ROUTES_PATH_HOME} />;
+};
 
 export default function () {
   return (
-    <BrowserRouter>
+    <BrowserRouter basename={ROUTES_BASENAME}>
       <Routes>
-        <Route path='/' element={<>home</>} />
-        <Route path='/about' element={<>about</>} />
+        <Route path={ROUTES_PATH_HOME} element={<>home</>} />
+        <Route path={ROUTES_PATH_SECTOR} element={<>sector</>} />
+        <Route path={ROUTES_PATH_ROOT} element={<RootNavigator />} />
+        <Route path='*' element={<RootNavigator />} />
       </Routes>
     </BrowserRouter>
   );
