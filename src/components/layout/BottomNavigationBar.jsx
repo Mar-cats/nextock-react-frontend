e11cdfd20774a39e33d6197d708e28840bf2d8b6
c9u11 <tjrdud6412@naver.com>
2025-02-08 23:20:36 +0900

    💄 하단 바 홈 아이콘 적용

--- src/components/layout/BottomNavigationBar.jsx
+++ src/components/layout/BottomNavigationBar.jsx
@@ -1,11 +1,24 @@
-import { Flex } from "@chakra-ui/react";
+import { Flex, IconButton, VStack } from "@chakra-ui/react";
 import { Link } from "react-router-dom";
 import { ROUTES_PATH_HOME } from "@/constants/routes";
+import { HiMiniHome } from "react-icons/hi2";
+import styled from "styled-components";
+
+const MenuSpan = styled.span`
+  font-size: 12px;
+`;
 
 const BottomNavigationBar = () => {
   return (
     <Flex justify='center' align='center' p={4}>
-      <Link to={ROUTES_PATH_HOME}>Home</Link>
+      <Link to={ROUTES_PATH_HOME}>
+        <IconButton variant={"ghost"} aria-label={"Home"}>
+          <VStack justify={"center"} align={"center"} gap={0}>
+            <HiMiniHome width={24} height={24} />
+            <MenuSpan>Home</MenuSpan>
+          </VStack>
+        </IconButton>
+      </Link>
     </Flex>
   );
 };
