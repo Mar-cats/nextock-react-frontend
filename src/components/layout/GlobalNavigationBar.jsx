b19fad729fd6402d4f2b56a93de1a4f6519de9d2
c9u11 <tjrdud6412@naver.com>
2025-02-08 23:31:21 +0900

    💄 상단 바 아이콘 적용

--- src/components/layout/GlobalNavigationBar.jsx
+++ src/components/layout/GlobalNavigationBar.jsx
@@ -1,12 +1,22 @@
-import { Flex } from "@chakra-ui/react";
+import { Flex, IconButton } from "@chakra-ui/react";
 import { Link } from "react-router-dom";
-import { ROUTES_PATH_SEARCH } from "@/constants/routes";
+import { ROUTES_PATH_HOME, ROUTES_PATH_SEARCH } from "@/constants/routes";
+import { LuSearch } from "react-icons/lu";
+import { BiLogoBaidu } from "react-icons/bi";
 
 const GlobalNavigationBar = () => {
   return (
     <Flex justify='space-between' align='center' p={4}>
-      <h1>Logo</h1>
-      <Link to={ROUTES_PATH_SEARCH}>Search</Link>
+      <Link to={ROUTES_PATH_HOME}>
+        <IconButton variant={"ghost"} aria-label='Home'>
+          <BiLogoBaidu />
+        </IconButton>
+      </Link>
+      <Link to={ROUTES_PATH_SEARCH}>
+        <IconButton variant={"ghost"} aria-label='Search'>
+          <LuSearch />
+        </IconButton>
+      </Link>
     </Flex>
   );
 };
