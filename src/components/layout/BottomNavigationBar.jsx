9d006ba1955c8ecd371dc277c3baedac29d8ea9a
c9u11 <tjrdud6412@naver.com>
2025-02-09 15:36:45 +0900

    💄 BottomNavigationBar Border 적용

--- src/components/layout/BottomNavigationBar.jsx
+++ src/components/layout/BottomNavigationBar.jsx
@@ -1,4 +1,4 @@
-import { Flex, IconButton, VStack } from "@chakra-ui/react";
+import { Center, IconButton, VStack } from "@chakra-ui/react";
 import { Link } from "react-router-dom";
 import { ROUTES_PATH_HOME } from "@/constants/routes";
 import { HiMiniHome } from "react-icons/hi2";
@@ -8,9 +8,36 @@ const MenuSpan = styled.span`
   font-size: 12px;
 `;
 
+const Container = styled(Center)`
+  position: relative;
+  padding: 8px 0;
+  &:before {
+    content: "";
+    position: absolute;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+    opacity: 0.2;
+    border: 1px solid transparent;
+    border-radius: 22px 22px 0 0;
+    background-image: linear-gradient(#fff, #fff),
+      linear-gradient(
+        135deg,
+        var(--color-color-black) 0%,
+        var(--color-gray-600) 18%,
+        var(--color-gray-900) 44%,
+        var(--color-gray-500) 96%,
+        var(--color-gray-700) 100%
+      );
+    background-origin: border-box;
+    background-clip: content-box, border-box;
+  }
+`;
+
 const BottomNavigationBar = () => {
   return (
-    <Flex justify='center' align='center' p={4}>
+    <Container>
       <Link to={ROUTES_PATH_HOME}>
         <IconButton variant={"ghost"} aria-label={"Home"}>
           <VStack justify={"center"} align={"center"} gap={0}>
@@ -19,7 +46,7 @@ const BottomNavigationBar = () => {
           </VStack>
         </IconButton>
       </Link>
-    </Flex>
+    </Container>
   );
 };
 
