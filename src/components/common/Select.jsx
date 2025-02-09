f5bd97e9b64033ca0ad3222b9ac50ea32ab9792b
c9u11 <tjrdud6412@naver.com>
2025-02-09 15:38:39 +0900

    💄 Select Component

---
+++ src/components/common/Select.jsx
@@ -0,0 +1,42 @@
+import { For, Stack, createListCollection } from "@chakra-ui/react";
+import {
+  SelectContent,
+  SelectItem,
+  SelectLabel,
+  SelectRoot,
+  SelectTrigger,
+  SelectValueText
+} from "@/components/ui/select";
+
+const Select = () => {
+  return (
+    <SelectRoot
+      collection={frameworks}
+      defaultValue={["day"]}
+      width='46px'
+      variant='filled'
+    >
+      <SelectTrigger>
+        <SelectValueText placeholder='Select Unit' />
+      </SelectTrigger>
+      <SelectContent>
+        {frameworks.items.map((unit) => (
+          <SelectItem item={unit} key={unit.value} p={2}>
+            {unit.label}
+          </SelectItem>
+        ))}
+      </SelectContent>
+    </SelectRoot>
+  );
+};
+
+const frameworks = createListCollection({
+  items: [
+    { label: "일", value: "day" },
+    { label: "주", value: "week" },
+    { label: "월", value: "month" },
+    { label: "년", value: "year" }
+  ]
+});
+
+export default Select;
