12ec74aca78077832ca994b84014c5c19a7d0ebc
c9u11 <tjrdud6412@naver.com>
2025-02-09 16:52:15 +0900

    💄 pillTags의 옵션을 변경할 수 있도록 prop 추가

--- src/components/common/Pilltags.jsx
+++ src/components/common/Pilltags.jsx
@@ -1,14 +1,15 @@
 import { Flex, Tag } from "@chakra-ui/react";
 
-const Tags = ({ tags, onClose }) => {
+const Tags = ({ tags, onClose, ...props }) => {
   return (
-    <Flex direction='row' gap='10px'>
+    <Flex gap='10px' wrap='wrap'>
       {tags.map((tag, index) => (
         <Tag.Root
           key={index}
           variant={"outline"}
           p={"6px 12px"}
           borderRadius={"full"}
+          {...props}
         >
           <Tag.Label width={"max-content"} fontSize={"12px"}>
             {tag}
