eeddfb9b2f80fb29c08d96b5fd7a35ca6b0951e0
c9u11 <tjrdud6412@naver.com>
2025-02-09 16:50:54 +0900

    💄 SubTitle Component

---
+++ src/components/common/SubTitle.jsx
@@ -0,0 +1,10 @@
+import styled from "styled-components";
+
+const SubTitle = styled.h1`
+  color: var(--color-color-black);
+  font-size: 14px;
+  font-weight: 600;
+  line-height: 22px;
+`;
+
+export default SubTitle;
