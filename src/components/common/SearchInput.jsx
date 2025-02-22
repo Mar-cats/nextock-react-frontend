cee7e74f0dde74ed546ab56d41a9ad8378001a89
c9u11 <tjrdud6412@naver.com>
2025-02-22 23:33:53 +0900

    ✨ 검색 기능 구현
    - 검색 시 parameter 변경
    - parameter가 있는 상태에서 검색 시 검색어가 기본으로 들어가도록 설정
    - 최근 검색어 추가 및 삭제 기능 구현

--- src/components/common/SearchInput.jsx
+++ src/components/common/SearchInput.jsx
@@ -2,27 +2,46 @@ import { Input } from "@chakra-ui/react";
 import { InputGroup } from "@/components/ui/input-group";
 import { LuSearch } from "react-icons/lu";
 import { IoCloseOutline } from "react-icons/io5";
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import { useSearchStore } from "@/store/search";
+import { ROUTES_PATH_SEARCH } from "@/constants/routes";
+import { useNavigate } from "react-router-dom";
 
 const SearchInput = () => {
   const [searchValue, setSearchValue] = useState("");
+  const { addSearch } = useSearchStore();
+  const navigate = useNavigate();
   const handleClear = () => setSearchValue("");
+  const handleChange = (e) => {
+    setSearchValue(e.target.value);
+  };
+  const handleKeyDown = (e) => {
+    if (e.code !== "Enter") return;
+    addSearch(e.target.value);
+    navigate(`${ROUTES_PATH_SEARCH}?q=${e.target.value}`);
+  };
+  useEffect(() => {
+    const urlParams = new URLSearchParams(window.location.search);
+    const query = urlParams.get("q");
+    setSearchValue(query);
+  }, []);
   return (
     <InputGroup
-      flex="1"
-      startElement={<LuSearch color="var(--color-gray-800)" />}
+      flex='1'
+      startElement={<LuSearch color='var(--color-gray-800)' />}
       endElement={
         searchValue && (
-          <IoCloseOutline onClick={handleClear} color="var(--color-gray-800)" />
+          <IoCloseOutline onClick={handleClear} color='var(--color-gray-800)' />
         )
       }
     >
       <Input
-        placeholder="검색어를 입력해주세요"
-        variant="subtle"
+        placeholder='검색어를 입력해주세요'
+        variant='subtle'
         color={"var(--color-color-black)"}
         value={searchValue}
-        onChange={(e) => setSearchValue(e.target.value)}
+        onChange={handleChange}
+        onKeyDown={handleKeyDown}
       />
     </InputGroup>
   );
--- src/pages/search/components/RecentSearch.jsx
+++ src/pages/search/components/RecentSearch.jsx
@@ -1,22 +1,18 @@
 import SubTitle from "@/components/common/SubTitle";
 import { VStack } from "@chakra-ui/react";
 import PillTags from "@/components/common/Pilltags";
+import { useSearchStore } from "@/store/search";
+import { useEffect } from "react";
 
 export default function () {
+  const { searches, getRecentSearches, removeSearch } = useSearchStore();
+  useEffect(() => {
+    getRecentSearches();
+  }, []);
   return (
     <VStack gap={"10px"} align={"flex-start"}>
       <SubTitle>최근 검색어</SubTitle>
-      <PillTags
-        tags={[
-          "최근 검색어",
-          "이렇게",
-          "하는 건",
-          "어때?",
-          "NVIDIA",
-          "길게 넘어가면 어떻게 되냐"
-        ]}
-        onClose={() => {}}
-      />
+      <PillTags tags={searches} onClose={(tag) => removeSearch(tag)} />
     </VStack>
   );
 }
