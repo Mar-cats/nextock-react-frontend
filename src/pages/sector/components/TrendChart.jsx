8655c623fb925d2f7619a3b60f36c3c1c7c804e0
c9u11 <tjrdud6412@naver.com>
2025-02-23 12:08:14 +0900

    ✨ 트렌드 그래프 1차 정리
    - 세로 축 표시 제거
    - 최고점 및 최저점 표시
    - 가로 축 포맷터 설정

--- src/pages/sector/components/TrendChart.jsx
+++ src/pages/sector/components/TrendChart.jsx
@@ -1,18 +1,37 @@
 import ApexCharts from "apexcharts";
 import { useEffect } from "react";
+import { toDateLabel, toMarkerLabel } from "@/utils/string";
 
 export default function () {
-  var options = {
+  const seriesData = [];
+  const date = new Date();
+  for (let i = 0; i < 30; i++) {
+    seriesData.push({
+      y: Math.floor(Math.random() * 400),
+      x: date.getTime()
+    });
+    date.setDate(date.getDate() + 1);
+  }
+  const maxPoint = seriesData.reduce((max, point) =>
+    point.y > max.y ? point : max
+  );
+  const minPoint = seriesData.reduce((min, point) =>
+    point.y < min.y ? point : min
+  );
+  const options = {
     chart: {
       type: "area",
       toolbar: {
         show: false
+      },
+      zoom: {
+        enabled: false
       }
     },
     series: [
       {
-        name: "sales",
-        data: [31, 40, 28, 51, 42, 109, 100]
+        name: "data",
+        data: seriesData
       }
     ],
     stroke: {
@@ -22,32 +41,77 @@ export default function () {
       enabled: false
     },
     xaxis: {
+      tickAmount: 3,
       type: "datetime",
-      categories: [
-        "2018-09-19T00:00:00.000Z",
-        "2018-09-19T01:30:00.000Z",
-        "2018-09-19T02:30:00.000Z",
-        "2018-09-19T03:30:00.000Z",
-        "2018-09-19T04:30:00.000Z",
-        "2018-09-19T05:30:00.000Z",
-        "2018-09-19T06:30:00.000Z"
-      ]
+      labels: {
+        formatter: toDateLabel
+      }
+    },
+    yaxis: {
+      show: false
+    },
+    grid: {
+      row: {
+        colors: ["transparent"],
+        opacity: 0.5
+      }
     },
     fill: {
       type: "gradient",
+      colors: ["#95A6FF"],
       gradient: {
-        shade: "dark",
+        shade: "light",
         type: "vertical"
       }
+    },
+    annotations: {
+      points: [
+        {
+          x: maxPoint.x,
+          y: maxPoint.y,
+          marker: {
+            size: 6,
+            fillColor: "#95A6FF",
+            strokeColor: "#fff"
+          },
+          label: {
+            text: toMarkerLabel(maxPoint),
+            style: {
+              background: "#fff",
+              color: "#333",
+              borderRadius: 6,
+              border: "none"
+            }
+          }
+        },
+        {
+          x: minPoint.x,
+          y: minPoint.y,
+          marker: {
+            size: 6,
+            fillColor: "#95A6FF",
+            strokeColor: "#fff"
+          },
+          label: {
+            text: toMarkerLabel(minPoint),
+            style: {
+              background: "#fff",
+              color: "#333",
+              borderRadius: 6,
+              border: "none"
+            },
+            offsetY: 10
+          }
+        }
+      ]
     }
   };
-
   useEffect(() => {
-    var chart = new ApexCharts(document.querySelector("#chart"), options);
+    const chart = new ApexCharts(document.querySelector("#chart"), options);
     chart.render();
     return () => {
       chart.destroy();
     };
   }, []);
-  return <div id="chart"></div>;
+  return <div id='chart'></div>;
 }
--- src/utils/string.jsx
+++ src/utils/string.jsx
@@ -13,4 +13,25 @@ const unitToString = (unit) => {
   }
 };
 
-export { unitToString };
+const toDateLabel = (dateString) => {
+  const date = new Date(dateString);
+  return [
+    `${date.getFullYear()}`,
+    `${new Intl.DateTimeFormat("en-US", { month: "short" }).format(
+      date
+    )} ${String(date.getDate()).padStart(2, "0")}`
+  ];
+};
+
+const toMarkerLabel = (data) => {
+  const { x, y } = data;
+  const date = new Date(x);
+  return [
+    `${String(date.getMonth() + 1).padStart(2, "0")}/${String(
+      date.getDate()
+    ).padStart(2, "0")}`,
+    `${y}회`
+  ];
+};
+
+export { unitToString, toDateLabel, toMarkerLabel };
