5869a0ed94705c8f17230bcf2eb20656b6e2e8a6
c9u11 <tjrdud6412@naver.com>
2025-03-05 23:04:21 +0900

    💄 SearchInput Component 디자인 수정
    - background color
    - placeholder color
    - border radius

--- src/components/common/SearchInput.jsx
+++ src/components/common/SearchInput.jsx
@@ -6,7 +6,7 @@ import { useEffect, useState } from "react";
 import { useSearchStore } from "@/store/search";
 import { ROUTES_PATH_SEARCH } from "@/constants/routes";
 import { useNavigate } from "react-router-dom";
-import { useSearchFocusStore } from '@/store/search';
+import { useSearchFocusStore } from "@/store/search";
 
 const SearchInput = () => {
   const [searchValue, setSearchValue] = useState("");
@@ -16,7 +16,7 @@ const SearchInput = () => {
   const handleClear = () => {
     setSearchValue("");
     onFocus();
-  }
+  };
   const handleChange = (e) => {
     setSearchValue(e.target.value);
   };
@@ -33,20 +33,23 @@ const SearchInput = () => {
   }, []);
   return (
     <InputGroup
-      flex='1'
-      startElement={<LuSearch color='var(--color-gray-800)' />}
+      flex="1"
+      startElement={<LuSearch color="var(--color-gray-800)" />}
       endElement={
         searchValue && (
-          <IoCloseOutline onClick={handleClear} color='var(--color-gray-800)' />
+          <IoCloseOutline onClick={handleClear} color="var(--color-gray-800)" />
         )
       }
     >
       <Input
-        placeholder='검색어를 입력해주세요'
-        variant='subtle'
+        placeholder="검색어를 입력해주세요"
+        variant="subtle"
         color={"var(--color-color-black)"}
+        background={"var(--color-gray-100)"}
+        _placeholder={{ color: "var(--color-gray-600)" }}
         border={"none"}
         outline={"none"}
+        borderRadius="12px"
         value={searchValue}
         onChange={handleChange}
         onKeyDown={handleKeyDown}
